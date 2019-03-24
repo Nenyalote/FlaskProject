99b620c8d8b9a65dcf963d28e87fb10472e591a9
Nenyalote <48046110+Nenyalote@users.noreply.github.com>
2019-03-24 22:59:35 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6393fd014b7a0e696895d5ee50fb55ec'
 
 posts = [
     {
@@ -24,6 +27,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title = 'Register', form=form)
+
+@app.route("/Login")
+def Login():
+    form = LoginForm()
+    return render_template('login.html',title = 'Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True,host='127.0.0.1', port=5000)
