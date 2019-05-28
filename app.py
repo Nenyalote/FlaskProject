50651fcfc52a45959042f8862d1db4d378673cb4
Nenyalote <48046110+Nenyalote@users.noreply.github.com>
2019-05-28 23:07:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,51 +1,56 @@
-from flask import Flask, render_template,url_for,flash,redirect
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '6393fd014b7a0e696895d5ee50fb55ec'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author':'Nenyalote',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'March 24,2019'
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Tanya',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'March 25,2019'
+        'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return  render_template('home.html',posts=posts)
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title='About')
+    return render_template('about.html', title='About')
 
-@app.route("/register",methods=['GET','POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
-        return redirect (url_for('home'))
-    return render_template('register.html',title = 'Register', form=form)
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login",methods=['GET','POST'])
-def Login():
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!','success')
+            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Pease check your email and password', 'danger')
-    return render_template('login.html',title = 'Login', form=form)
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
-if __name__ == "__main__":
-    app.run(debug=True,host='127.0.0.1', port=5000)
+
+if __name__ == '__main__':
+    app.run(debug=True)
